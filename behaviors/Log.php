5004552c9e056339229d1f291a83f05f82bb85b3
Mikhail Eremeev <velonski@yandex.ru>
2016-01-21 10:02:39 +0500

    remove die() for debug

--- behaviors/Log.php
+++ behaviors/Log.php
@@ -130,7 +130,6 @@ class Log extends Behavior
         $old = $this->owner->oldAttributes;
         $diff = array_diff_assoc($new,$old);
         $this->_changed_attributes = $diff;
-        die( var_dump($diff) );
         if(count($diff) > 0) {
             $this->owner->{$this->timeField} = date('Y-m-d H:i:sP');
             $this->_to_save_log = true;
