2aa1f94dc8a4f9fc0e0907b20eca4b16d9c77134
k1sliy <maks280795@mail.ru>
2016-03-04 11:15:57 +0500

    Code cleanup

--- behaviors/Log.php
+++ behaviors/Log.php
@@ -2,8 +2,12 @@
 namespace log\behaviors;
 
 use Yii;
-use yii\db\ActiveRecord;
 use yii\base\Behavior;
+use yii\base\ErrorException;
+use yii\base\Event;
+use Yii\console\Application;
+use yii\db\ActiveQueryInterface;
+use yii\db\ActiveRecord;
 use yii\db\AfterSaveEvent;
 use yii\db\StaleObjectException;
 use yii\validators\Validator;
@@ -12,17 +16,21 @@ use yii\validators\Validator;
  * Behavior class for logging all changes to a log table.
  * It requires a log model. By default it is owner class name + 'Log'.
  *
- * @prop array  $logAttributes          A list of all attributes to be saved in the log table
- * @prop string $logClass               Class name for the log model
- * @prop string $changedAttributesField Field in the table to store changed attributes list. Default: changed_attributes
- * @prop string $changedByField         Field in the table to store the author of the changes (Yii::$app->user->id). Default: changed_by
- * @prop string $timeField              Field where the time of last change is stored. Default: atime
- *
+ * @property array  $logAttributes          A list of all attributes to be saved in the log table
+ * @property string $logClass               Class name for the log model
+ * @property string $changedAttributesField Field in the table to store changed attributes list. Default: changed_attributes
+ * @property string $changedByField         Field in the table to store the author of the changes (Yii::$app->user->id). Default: changed_by
+ * @property string $timeField              Field where the time of last change is stored. Default: atime
  */
 class Log extends Behavior
 {
     /**
-     * A list of all attributes to be saved in the log table
+     * @var ActiveRecord the owner of this behavior
+     */
+    public $owner;
+
+    /**
+     * array A list of all attributes to be saved in the log table
      */
     public $logAttributes;
 
@@ -36,9 +44,9 @@ class Log extends Behavior
      * Default: 'changed_by'
      */
     public $changedByField = 'changed_by';
-    
+
     /**
-     * Value for 'changedByField', needed in console mode of Yii2 application 
+     * Value for 'changedByField', needed in console mode of Yii2 application
      */
     public $changedByValue;
 
@@ -78,64 +86,68 @@ class Log extends Behavior
 
     /**
      * @inherit
+     * @param ActiveRecord $owner
+     * @throws ErrorException
      */
     public function attach($owner)
     {
-        if(is_null($this->logClass)) {
-            $this->logClass = $owner->className()."Log";
+        if (is_null($this->logClass)) {
+            $this->logClass = $owner->className() . "Log";
         }
         if (!class_exists($this->logClass, false)) {
-            throw new ErrorException('Model for logging "'.$this->logClass.'" ');
+            throw new ErrorException('Model for logging "' . $this->logClass . '" ');
         }
 
-        if(isset($this->versionField) && !$owner->isNewRecord) {
+        if (isset($this->versionField) && !$owner->isNewRecord) {
 //            die('new: '.$owner->isNewRecord);
-            $owner->validators[] = Validator::createValidator('required',$owner,$this->versionField,[]);
-            $owner->validators[] = Validator::createValidator('string',$owner,$this->versionField,['max' => '50']);
-            $owner->validators[] = Validator::createValidator('match',$owner,$this->versionField,['pattern' => '/^[0-9]+$/']);
+            $owner->validators[] = Validator::createValidator('required', $owner, $this->versionField, []);
+            $owner->validators[] = Validator::createValidator('string', $owner, $this->versionField, ['max' => '50']);
+            $owner->validators[] = Validator::createValidator('match', $owner, $this->versionField, ['pattern' => '/^[0-9]+$/']);
         }
-        return parent::attach($owner);
+        parent::attach($owner);
     }
 
     /**
      * Saves a record to the log table
      *
-     * @param \yii\db\AfterSaveEvent $event
+     * @param null $attributes
+     * @return ActiveQueryInterface
+     * @internal param AfterSaveEvent $event
      */
-    public function getLogged($attributes = NULL)
+    public function getLogged($attributes = null)
     {
         $attr = [];
-        if(is_string($attributes)) {
+        if (is_string($attributes)) {
             $attr[] = $attributes;
-        }elseif(is_array($attributes)) {
+        } elseif (is_array($attributes)) {
             $attr = $attributes;
         }
 
-        if(count($attr) > 0) {
-            $array = '{'.implode(',',$attr).'}';
+        if (count($attr) > 0) {
+            $array = '{' . implode(',', $attr) . '}';
         } else {
-            $array = NULL;
+            $array = null;
         }
         return $this->owner
-            ->hasMany($this->logClass,['doc_id' => 'id'])
-            ->andFilterWhere(['&&',$this->changedAttributesField,$array])
-        ;
+            ->hasMany($this->logClass, ['doc_id' => 'id'])
+            ->andFilterWhere(['&&', $this->changedAttributesField, $array]);
     }
 
     /**
      * Sets the time of change
      *
-     * @param \yii\db\BeforeSaveEvent $event
+     * @param Event $event
+     * @throws StaleObjectException
      */
     public function logBeforeSave($event)
     {
         // Computing attributes to save (diff)
-        $attributes = array_diff($this->logAttributes,[$this->timeField]);
+        $attributes = array_diff($this->logAttributes, [$this->timeField]);
         $new = $this->owner->getDirtyAttributes($attributes);
         $old = $this->owner->oldAttributes;
-        $diff = array_diff_assoc($new,$old);
+        $diff = array_diff_assoc($new, $old);
         $this->_changed_attributes = $diff;
-        if(count($diff) > 0) {
+        if (count($diff) > 0) {
             $this->owner->{$this->timeField} = date('Y-m-d H:i:sP');
             $this->_to_save_log = true;
         } else {
@@ -143,10 +155,10 @@ class Log extends Behavior
         }
 
         // Check current version of the record before update
-        if(isset($this->versionField)) {
-            if($event->name === 'beforeUpdate') {
+        if (isset($this->versionField)) {
+            if ($event->name === 'beforeUpdate') {
                 $row = $this->owner->find()->where($this->owner->getPrimaryKey(true))->select($this->versionField)->asArray()->one();
-                if(isset($row[$this->versionField]) && (string)$row[$this->versionField] !== (string)$this->owner->getAttribute($this->versionField)) {
+                if (isset($row[$this->versionField]) && (string)$row[$this->versionField] !== (string)$this->owner->getAttribute($this->versionField)) {
                     throw new StaleObjectException('The object being updated is outdated.');
                 }
             }
@@ -161,31 +173,33 @@ class Log extends Behavior
      * Saves a record to the log table
      *
      * @param \yii\db\AfterSaveEvent $event
+     * @throws ErrorException
      */
     public function logAfterSave($event)
     {
-        if(! $event instanceof AfterSaveEvent) return;
-        if(is_null($event->changedAttributes)) return;
-        if(! $this->_to_save_log) return;
+        if (!$event instanceof AfterSaveEvent) return;
+        if (is_null($event->changedAttributes)) return;
+        if (!$this->_to_save_log) return;
 
         $attributes = $this->owner->getAttributes($this->logAttributes);
         $attributes['doc_id'] = $attributes['id'];
         unset($attributes['id']);
-        $attributes[$this->changedAttributesField] = '{'.implode(',',array_keys($this->_changed_attributes)).'}';
+        $attributes[$this->changedAttributesField] = '{' . implode(',', array_keys($this->_changed_attributes)) . '}';
 
-        if($this->changedByField) {
-            if( Yii::$app instanceof \Yii\console\Application ) {
-                $attributes[$this->changedByField] = NULL;
+        if ($this->changedByField) {
+            if (Yii::$app instanceof Application) {
+                $attributes[$this->changedByField] = null;
             } else {
-                $attributes[$this->changedByField] = Yii::$app->user->isGuest ? NULL : Yii::$app->user->id;
+                $attributes[$this->changedByField] = Yii::$app->user->isGuest ? null : Yii::$app->user->id;
             }
         }
 
         $logClass = $this->logClass;
+        /** @var ActiveRecord $log */
         $log = new $logClass($attributes);
 
-        if(! $log->save()) {
-            throw new ErrorException(print_r($log->errors,true));
+        if (!$log->save()) {
+            throw new ErrorException(print_r($log->errors, true));
         }
     }
 }
